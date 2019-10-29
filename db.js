12fc874e9e7de379835440affc8a3185e3cdae4a
nirustanite <nirustanite@gmail.com>
2019-10-29 03:23:49 +0100

    Setup Db connection

---
+++ db.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+const connectonString = `postgres://postgres:secret@localhost:5432/postgres`
+
+const db = new Sequelize(connectonString);
+
+db.sync({force: false})
+  .then(() => console.log("Db schema updated"))
+  .catch(console.error())
+
+module.exports = db;
