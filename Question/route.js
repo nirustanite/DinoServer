686debcc1a66f4acf3aa1b6c5315ac62ae66bf69
nirustanite <nirustanite@gmail.com>
2019-10-31 14:57:28 +0100

    Implemented Routes

--- Question/route.js
+++ Question/route.js
@@ -9,57 +9,6 @@ router.get('/question', async (ctx,next) => {
 
 
 
-// router.get('/question/:id', (ctx, next) => {
-//     return Question.findByPk(ctx.params.id)
-//         .then(question => {
-//             ctx.body = question;
-//             next()
-//         })
-//         .catch(err => {
-//             ctx.throw(404, err);
-//         })
-// })
-
-// router.get('/question', (ctx, next) => {
-//     return Question.findAll({
-//         where: {
-//             id: {
-//                 [Op.gt]: 1
-//             }
-//         }
-//     })
-//         .then(questions => {
-//             ctx.body = questions;
-//             next()
-//         })
-//         .catch(err => {
-//             ctx.throw(404, err);
-//         })
-// })
-
-
-// router.put("/question/:id", (ctx, next) => {
-//     return Question.findByPk(ctx.params.id)
-//         .then(question => {
-//             return question.update(ctx.request.body)
-//                 .then(async () => {
-//                     if (question.id === 1) {
-//                         await createQuestionfrom2()
-//                     }
-//                     ctx.body = {
-//                         message: "Answer Updated successfully"
-//                     }
-//                     console.log('ctx', ctx.body)
-//                     next()
-//                 })
-//                 .catch(err => {
-//                     console.log('hello')
-//                     // ctx.throw(404, err)
-//                 })
-//         })
-// })
-
-
 
 module.exports = router;
 
